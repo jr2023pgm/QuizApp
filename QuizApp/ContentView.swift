353d5e31a22d3c0fa9f72c82323e04077a4cf1e0
Jay Ram <jr2023pgm@gmail.com>
2023-07-09 09:42:55 +0400

    Added Jokes

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var questions = [Question(question: "Which is the richest company in the world?", option1: "Samsung", option2: "Apple", option3: "Microsoft", option4: "Tesla"),
+                    Question(question: "What framework are we using?", option1: "UIKit", option2: "SwiftUI", option3: "React Native", option4: "Flutter"),
+                    Question(question: "Who is the first man on the moon?", option1: "James A Lovell Jr.", option2: "Michael Collins", option3: "Charles Lindbergh", option4: "Neil Armstrong")]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ QuizApp/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by jr on 09/07/2023.
+//
+
+import Foundation
+
+struct Question {
+    var question: String
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+}
